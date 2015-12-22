f03eab26212b14728ed4ceafd01db6b487d3ea56
estroid <estroid@gmail.com>
2015-12-21 20:52:13 -0500

    add hadouken

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
-// "Finally, hide div.ryu-ready and show div.ryu-throwing to preview that image.
+$(document).ready(function() {
+    $('.ryu').mouseenter(function() {
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-ready').hide();
+        $('.ryu-still').show();
+    })
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function() {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 
-// $('.ryu-ready').hide();
-// $('.ryu-throwing').show();
+});
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+

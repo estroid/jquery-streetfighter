5232ce48408bececebfe4bc5efb708f2d2efc8a2
estroid <estroid@gmail.com>
2016-01-01 14:22:05 -0500

    add text and jquery fokeydown

--- js/app.js
+++ js/app.js
@@ -22,9 +22,28 @@ $(document).ready(function() {
     .mouseup(function() {
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
-    });
+    })
 
 });
+
+// // Ryu to switch to his "looking cool" pose when the user presses the “x” key. When the user releases “x”, Ryu should go back to standing still.
+// hints: when you use the keydown method, you need to check to see if the key that the user pressed down is the specific one you’re looking for. The key code for “x” is 88.
+
+    $(document).keydown(function(event) {
+        if (event.keyCode == 88) {
+            console.log('and');
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.ryu-cool').show();
+      }
+    });
+    $(document).keyup(function(event) {
+        if (event.keyCode == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
+    });
 
 function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
